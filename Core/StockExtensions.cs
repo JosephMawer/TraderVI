7fc59b07e462b85dfa779f81333b26756a0bfe7d
Joseph Mawer <joseph.mawer@idea.cwi.local>
2019-10-06 22:05:30 -0400

    CalculateEMA() method was added; convert for to linq visual studio refactoring was used in the algorithm that calculates the EMA

--- Core/StockExtensions.cs
+++ Core/StockExtensions.cs
@@ -22,7 +22,55 @@ namespace Core
             for (int i = 0; i < stockInfo.Count - period + 1; i++) 
                 yield return new SMA(stockInfo.Skip(i).Take(period).Average(stock => stock.Close), 
                     DateTime.Parse(stockInfo[i].TimeOfRequest));
-            
+        }
+
+        public static IEnumerable<EMA> CalculateEMA(this List<IStockInfo> stockInfo, int period)
+        {
+            //https://www.iexplain.org/ema-how-to-calculate/
+            // 
+            // todo: calculate the entire EMA for a specific stock, but only return the period requested - this might give more accurate numbers
+
+            var range = stockInfo.GetRange(0, period);
+            decimal alpha = 2m / (period + 1m);
+
+       
+            var emaList = new List<EMA>();
+            var count = 0;
+            bool firstPass = true;
+
+            #region original version
+            //foreach (var stock in Enumerable.Reverse(range))    // reverse date as it is from newest to oldest
+            //{
+            //    var previousEMA = (firstPass) ?
+            //        CalculateSMA(stockInfo, 50).ToList()[period + 1].Price :
+            //        emaList[count - 1].Price;
+
+            //    firstPass = false;
+
+            //    var priceToday = stock.Close;
+            //    var price = priceToday * alpha + previousEMA * (1 - alpha);
+            //    var ema = new EMA(price, DateTime.Parse(stock.TimeOfRequest));
+            //    emaList.Add(ema);
+            //    count++;
+            //}
+            #endregion
+
+            // linq version!!
+            foreach (var (stock, previousEMA) in from stock in Enumerable.Reverse(range)// reverse date as it is from newest to oldest
+                                                 let previousEMA = (firstPass) ?
+                                                                     CalculateSMA(stockInfo, 50).ToList()[period + 1].Price :
+                                                                     emaList[count - 1].Price
+                                                 select (stock, previousEMA))
+            {
+                firstPass = false;
+                var priceToday = stock.Close;
+                var price = priceToday * alpha + previousEMA * (1 - alpha);
+                var ema = new EMA(price, DateTime.Parse(stock.TimeOfRequest));
+                emaList.Add(ema);
+                count++;
+            }
+
+            return emaList;
         }
 
         /// <summary>
