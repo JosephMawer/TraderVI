784a49959507933b60979a0e9739168a7332e913
Joseph Mawer <joseph.mawer@caseware.com>
2019-10-08 22:48:41 -0400

    Added a 'SafeGetString' extension method in the stock db project

---
+++ StocksDB/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace StocksDB
+{
+    public static class Extensions
+    {
+        public static string SafeGetString(this SqlDataReader reader, int colIndex)
+        {
+            if (!reader.IsDBNull(colIndex))
+                return reader.GetString(colIndex);
+            return string.Empty;
+        }
+    }
+}
