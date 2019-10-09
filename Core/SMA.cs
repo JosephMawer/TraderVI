90bb4f5131a1dbb907fea23b28f2dfb4fbb815b4
Joseph Mawer <joseph.mawer@caseware.com>
2019-10-08 22:49:17 -0400

    Added SMA struct

--- Core/SMA.cs
+++ Core/SMA.cs
@@ -2,12 +2,23 @@
 
 namespace Core
 {
-
+    // Created SMA and EMA structs, even though they are identical, just for readability purposes
     public struct SMA
     {
         public readonly decimal Price;
         public readonly DateTime Date;
         public SMA(decimal price, DateTime date)
+        {
+            Price = price;
+            Date = date;
+        }
+    }
+
+    public struct EMA
+    {
+        public readonly decimal Price;
+        public readonly DateTime Date;
+        public EMA(decimal price, DateTime date)
         {
             Price = price;
             Date = date;
