2a64d9121eea0641ed0b2b7cf1f1296f6a4a7694
Joseph Mawer <joseph.mawer@caseware.com>
2019-10-08 22:52:42 -0400

    Inital thoughts/structure for how alert rules might work

--- Core/Alerts/AlertRules.cs
+++ Core/Alerts/AlertRules.cs
@@ -6,5 +6,27 @@ namespace Core.Alerts
 {
     public class AlertRules
     {
+        /// <summary>
+        /// Checks if SMA(x) has crossed over SMA(y)
+        /// </summary>
+        public static void CheckForMovingAverageCrossOver()
+        {
+
+        }
+
+        /// <summary>
+        /// Checks if price is close to the SMA
+        /// </summary>
+        public static void IsPriceCloseToSMA()
+        {
+
+        }
+        /// <summary>
+        /// Checks if price has crossed the SMA
+        /// </summary>
+        public static void HasPriceCrossedOverSMA()
+        {
+
+        }
     }
 }
