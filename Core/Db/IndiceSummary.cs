315fa40604af98d9f263846fe1fe3a4162439f3c
Joseph Mawer <joseph.mawer@hotmail.com>
2026-04-16 23:34:09 -0400

    Refactor: Modernize IndiceSummary DB Operations

--- Core/Db/IndiceSummary.cs
+++ Core/Db/IndiceSummary.cs
@@ -1,190 +1,157 @@
 ﻿using Core.TMX.Models;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using Microsoft.Data.SqlClient;
-
-//using Microsoft.Data.SqlClient;
 using System.Threading.Tasks;
 
-namespace Core.Db
+namespace Core.Db;
+
+/// <summary>
+/// Represents information about a single index for a given day.
+/// </summary>
+public struct IndiceValue
 {
+    public DateTime Date { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public decimal Change { get; set; }
+    public decimal PercentChange { get; set; }
+}
+
+public enum Indices
+{
+    TSX,
+    TSX_Venture,
+    VIXC,
+    Energy,
+    Financials,
+    Health_Care,
+    Industrials,
+    Info_Tech,
+    Metals_Mining,
+    Telecom,
+    Utilities,
+    Materials,      // Added for sector index parity with TMX ^TTMT
+    Technology,     // Added for sector index parity with TMX ^TTTK
+}
+
+/// <summary>
+/// Data access for the [dbo].[IndiceSummary] table.
+/// Modernized: parameterized queries, proper async, no string interpolation in SQL.
+/// </summary>
+public class IndiceSummary : SQLBase
+{
+    #region Inserts
 
     /// <summary>
-    /// Represents information about a single index for a given day
+    /// Upserts a single index summary entry.
     /// </summary>
-    public struct IndiceValue
+    public async Task InsertIndiceSummaryAsync(MarketIndices index)
     {
-        public DateTime Date { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public decimal Change { get; set; }
-        public decimal PercentChange { get; set; }
-    }
-   
+        const string sql = """
+            MERGE [dbo].[IndiceSummary] AS target
+            USING (VALUES (@Date, @Name, @Last, @Change, @PercentChange))
+                AS source ([Date], [Name], [Last], [Change], [PercentChange])
+            ON target.[Date] = source.[Date] AND target.[Name] = source.[Name]
+            WHEN MATCHED THEN
+                UPDATE SET [Last] = source.[Last], [Change] = source.[Change], [PercentChange] = source.[PercentChange]
+            WHEN NOT MATCHED THEN
+                INSERT ([Date], [Name], [Last], [Change], [PercentChange])
+                VALUES (source.[Date], source.[Name], source.[Last], source.[Change], source.[PercentChange]);
+            """;
 
-    public enum Indices
+        await using var conn = new SqlConnection(ConnectionString);
+        await conn.OpenAsync();
+        await using var cmd = new SqlCommand(sql, conn);
+        cmd.Parameters.Add(new SqlParameter("@Date", SqlDbType.DateTime2) { Value = index.Date });
+        cmd.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar, 25) { Value = index.Name });
+        cmd.Parameters.Add(new SqlParameter("@Last", SqlDbType.Real) { Value = index.Last });
+        cmd.Parameters.Add(new SqlParameter("@Change", SqlDbType.Real) { Value = index.Change });
+        cmd.Parameters.Add(new SqlParameter("@PercentChange", SqlDbType.Real) { Value = index.PercentChange });
+        await cmd.ExecuteNonQueryAsync();
+    }
+
+    /// <summary>
+    /// Upserts a batch of index summary entries.
+    /// </summary>
+    public async Task InsertIndiceSummaryAsync(IList<MarketIndices> indexList)
     {
-        TSX,
-        TSX_Venture,
-        VIXC,
-        Energy,
-        Financials,
-        Health_Care,
-        Industrials,
-        Info_Tech,
-        Metals_Mining,
-        Telecom,
-        Utilities,
+        foreach (var index in indexList)
+            await InsertIndiceSummaryAsync(index);
     }
-    public class IndiceSummary : SQLBase
+
+    // Keep legacy overloads for backward compatibility until callers are migrated.
+    [Obsolete("Use InsertIndiceSummaryAsync instead.")]
+    public Task InsertIndiceSummary(MarketIndices index) => InsertIndiceSummaryAsync(index);
+
+    [Obsolete("Use InsertIndiceSummaryAsync instead.")]
+    public Task InsertIndiceSummary(IList<MarketIndices> indexList) => InsertIndiceSummaryAsync(indexList);
+
+    #endregion
+
+    /// <summary>
+    /// Gets the closing price for the specified index on the given date.
+    /// Returns 0 if no data found.
+    /// </summary>
+    public async Task<decimal> GetDailyAverage(Indices indice, DateTime date)
     {
-        #region Inserts
-        /// <summary>
-        /// Inserts Index summary information into database table IndiceSummary
-        /// </summary>
-        /// <param name="index"></param>
-        /// <returns></returns>
-        public async Task InsertIndiceSummary(MarketIndices index)
-        {
-            var sqlQueryStatement = $"insert into [Db].[dbo].[IndiceSummary] values (@date,@name,@last,@changed,@percentchanged)";
-            await base.Insert(sqlQueryStatement,
-                new List<SqlParameter>()
-                {
-                    new SqlParameter() { ParameterName = "@date", DbType = DbType.DateTime2, Value=index.Date},
-                    new SqlParameter() { ParameterName = "@name", DbType = DbType.String, Size = 25, Value = index.Name},
-                    new SqlParameter() { ParameterName = "@last", DbType = DbType.Single, Value = index.Last},
-                    new SqlParameter() { ParameterName = "@changed", DbType = DbType.Single, Value = index.Change},
-                    new SqlParameter() { ParameterName = "@percentchanged", DbType = DbType.Single, Value = index.PercentChange}
-                });
-        }
+        const string sql = """
+            SELECT [Last]
+            FROM [dbo].[IndiceSummary]
+            WHERE [Name] = @Name
+              AND [Date] >= @DateStart AND [Date] < @DateEnd
+            """;
 
-     
-        /// <summary>
-        /// Inserts Index summary information into database table IndiceSummary
-        /// </summary>
-        /// <param name="index"></param>
-        /// <returns></returns>
-        public async Task InsertIndiceSummary(IList<MarketIndices> indexList)
-        {
-            var sqlQueryStatement = $"insert into [Db].[dbo].[IndiceSummary] values (@date,@name,@last,@changed,@percentchanged)";
-            foreach (var index in indexList)
-            {
-                await base.Insert(sqlQueryStatement,
-                    new List<SqlParameter>()
-                    {
-                        new SqlParameter() { ParameterName = "@date", DbType = DbType.DateTime2, Value=index.Date},
-                        new SqlParameter() { ParameterName = "@name", DbType = DbType.String, Size = 25, Value = index.Name},
-                        new SqlParameter() { ParameterName = "@last", DbType = DbType.Single, Value = index.Last},
-                        new SqlParameter() { ParameterName = "@changed", DbType = DbType.Single, Value = index.Change},
-                        new SqlParameter() { ParameterName = "@percentchanged", DbType = DbType.Single, Value = index.PercentChange}
-                    });
-            }
-        }
-        #endregion
+        await using var conn = new SqlConnection(ConnectionString);
+        await conn.OpenAsync();
+        await using var cmd = new SqlCommand(sql, conn);
+        cmd.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar, 25) { Value = EnumToString(indice) });
+        cmd.Parameters.Add(new SqlParameter("@DateStart", SqlDbType.DateTime2) { Value = date.Date });
+        cmd.Parameters.Add(new SqlParameter("@DateEnd", SqlDbType.DateTime2) { Value = date.Date.AddDays(1) });
 
-    
-        /// <summary>
-        /// Gets the average price from database for the specified indice on the specified date
-        /// </summary>
-        /// <param name="indice">The indice (based on TSX)</param>
-        /// <param name="date">The date of the value you want to see</param>
-        /// <returns></returns>
-        public async Task<decimal> GetDailyAverage(Indices indice, DateTime date)
-        {
-            string query = $"SELECT [Last] " +
-                           $"FROM [Db].[dbo].[IndiceSummary] " +
-                           $"WHERE [Name] = '{EnumToString(indice)}' " +
-                           $"AND [Date] >= '" + date.ToShortDateString() + "' AND [Date] < '" + date.AddDays(1).ToShortDateString() + "'";
-
-
-            using (SqlConnection con = new SqlConnection(ConnectionString))
-            {
-                try
-                {
-                    await con.OpenAsync();
-                    using (var cmd = new SqlCommand(query, con))
-                    {
-                        using (var reader = await cmd.ExecuteReaderAsync())
-                        {
-                            try
-                            {
-                                if (!reader.HasRows) return 0m;
-                                while (await reader.ReadAsync())
-                                {
-                                    var average = reader.GetDecimal(0);
-
-                                    return average;
-                                }
-                            }
-                            catch (Exception ex)
-                            {
-                                // should probably figure out a better way to handle this, but for now if we don't have
-                                // the average for a particualr date just return 0
-                                return 0m;
-                            }
-                          
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    var msg = ex.Message;
-                }
-
-            }
-
-            throw new Exception("Where's bobs wife?");
-        }
-        public async Task<IndiceValue> GetDailyMarketAverage(Indices indice)
-        {
-            string query = $"SELECT TOP(1) [Date],[Name],[Last],[Change],[PercentChange] " +
-                           $"FROM[Db].[dbo].[IndiceSummary] " +
-                           $"where [Name] = '{EnumToString(indice)}' " +
-                           $"Order By[Date] DESC";
-
-
-            using (SqlConnection con = new SqlConnection(ConnectionString))
-            {
-                try
-                {
-                    await con.OpenAsync();
-                    using (SqlCommand cmd = new SqlCommand(query, con))
-                    {
-                        using (var reader = await cmd.ExecuteReaderAsync())
-                        {
-                            while (await reader.ReadAsync())
-                            {
-                                var index = new IndiceValue();
-                                index.Date = reader.GetDateTime(0);
-                                index.Name = reader.GetString(1);
-                                index.Price = reader.GetDecimal(2);
-                                index.Change = reader.GetDecimal(3);
-                                index.PercentChange = reader.GetDecimal(4);
-
-                                return index;
-                            }
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    var msg = ex.Message;
-                }
-
-            }
-
-            throw new Exception("Where's bob?");
-            
-        }
-
-        private string EnumToString(Indices indice)
-        {
-            if (indice == Indices.Metals_Mining)
-                return "Metals & Mining";   // special case for metals and mining
-            else return indice.ToString().Replace("_", " "); // otherwise simply replace underscores with original spaces 
-        }
-
-
+        var result = await cmd.ExecuteScalarAsync();
+        return result is decimal d ? d : (result != null ? Convert.ToDecimal(result) : 0m);
     }
+
+    /// <summary>
+    /// Gets the most recent index value for the specified index.
+    /// </summary>
+    public async Task<IndiceValue> GetDailyMarketAverage(Indices indice)
+    {
+        const string sql = """
+            SELECT TOP(1) [Date], [Name], [Last], [Change], [PercentChange]
+            FROM [dbo].[IndiceSummary]
+            WHERE [Name] = @Name
+            ORDER BY [Date] DESC
+            """;
+
+        await using var conn = new SqlConnection(ConnectionString);
+        await conn.OpenAsync();
+        await using var cmd = new SqlCommand(sql, conn);
+        cmd.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar, 25) { Value = EnumToString(indice) });
+
+        await using var reader = await cmd.ExecuteReaderAsync();
+        if (await reader.ReadAsync())
+        {
+            return new IndiceValue
+            {
+                Date = reader.GetDateTime(0),
+                Name = reader.GetString(1),
+                Price = Convert.ToDecimal(reader.GetValue(2)),
+                Change = Convert.ToDecimal(reader.GetValue(3)),
+                PercentChange = Convert.ToDecimal(reader.GetValue(4)),
+            };
+        }
+
+        throw new InvalidOperationException($"No data found for index '{EnumToString(indice)}'.");
+    }
+
+    private static string EnumToString(Indices indice) => indice switch
+    {
+        Indices.Metals_Mining => "Metals & Mining",
+        Indices.Health_Care => "Health Care",
+        Indices.Info_Tech => "Info Tech",
+        _ => indice.ToString().Replace("_", " "),
+    };
 }
